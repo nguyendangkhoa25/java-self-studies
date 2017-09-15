66b45515b97d23c18bc86984b322b4a01d45df06
knguyen <ndangkhoa@tma.com.vn>
2017-09-15 16:22:25 +0700

    draw commit

---
+++ java-core/java-collections/src/main/java/com/khoa/nguyen/self/studies/collections/JavaArrayList.java
@@ -0,0 +1,13 @@
+package com.khoa.nguyen.self.studies.collections;
+
+import java.util.ArrayList;
+
+public class JavaArrayList {
+
+    public static void main(String args[]) {
+        ArrayList<String> stringArrayList = new ArrayList<String>();
+        stringArrayList.add("Dog");
+
+    }
+
+}
