11872a6bf3d4089bdc4e583768b3630c3b43fd53
knguyen <ndangkhoa@tma.com.vn>
2017-09-15 17:12:48 +0700

    commit

--- java-core/java-collections/src/main/java/com/khoa/nguyen/self/studies/collections/JavaArrayList.java
+++ java-core/java-collections/src/main/java/com/khoa/nguyen/self/studies/collections/JavaArrayList.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 public class JavaArrayList {
 
     public static void main(String args[]) {
+        //Create an ArrayList by default constructor.
         ArrayList<String> stringArrayList = new ArrayList<String>();
         stringArrayList.add("Dog");
 
